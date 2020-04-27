d4dc472d9dac9660f5fda62a6e5a037a37f91b6a
plasma parse <mk_vertex3625_plasma@kcn.jp>
2020-04-27 14:19:31 +0900

    hello worldを習得

--- hello_world/target/debug/deps/hello_world-272e2cb6005cd3b1.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/mk_ve/source/repos/RustPractice/hello_world/target/debug/deps/hello_world-272e2cb6005cd3b1: src/main.rs
-
-/mnt/c/Users/mk_ve/source/repos/RustPractice/hello_world/target/debug/deps/hello_world-272e2cb6005cd3b1.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/mk_ve/source/repos/RustPractice/hello_world/target/debug/hello_world: /mnt/c/Users/mk_ve/source/repos/RustPractice/hello_world/src/main.rs
